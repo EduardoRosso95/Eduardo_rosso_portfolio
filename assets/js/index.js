bc9c90d95512ae459212590de3ae9fefec522042
EduardoRosso95 <eduardo.rossoo@gmail.com>
2025-03-03 20:24:23 +0100

    End of mobile version

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,10 +1,22 @@
 function sendMail(){
-    let parms = {
-       
-        email : document.getElementById('email').value,
-        message : document.getElementById('message').value,
-        
-    };
 
-    emailjs.send("service_3bk0j9n", "template_719ywsw", parms).then(alert("Email sent successfully!")); 
+    const emailField = document.getElementById('email');
+    const messageField = document.getElementById('message');
+
+    //verifico che i campi siano validi, se lo sono procedon con l'invio della mail
+    if (emailField.validity.valid && messageField.validity.valid) {
+
+        let parms = {
+       
+            email : document.getElementById('email').value,
+            message : document.getElementById('message').value,
+        
+        };
+
+        emailjs.send("service_3bk0j9n", "template_719ywsw", parms).then(alert("Email mandata correttamente!")); 
+        
+        messageField.value = '';
+
+    }
 };
+  
